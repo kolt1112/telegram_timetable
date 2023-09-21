5d388074e193a30b55c8b997c9aa28e30664df53
kolt1112 <gadarin784@gmail.com>
2023-09-21 18:58:41 +0300

    add input for choosing day and classes

--- parser.py
+++ parser.py
@@ -7,6 +7,12 @@ book = openpyxl.load_workbook('./data/timetable_april.xlsx')
 print('Введите день, на которое вы хотите узнать расписание\n')
 number = int(input())
 
+if int(number) < 9:
+    number += 1
+elif int(number) >= 15:
+    number -= 1
+
+
 
 print("Напишите название класса в котором вы учитесь 😉")
 your_class = input()
@@ -15,6 +21,8 @@ sheet = book.worksheets[number]
 #sheet = book['17']
 number_rows = sheet.max_row
 number_columns = sheet.max_column
+
+
 
 
 def get_merged_cell_value(sheet, cell):
@@ -30,26 +38,65 @@ def find_class_cell(value):
                 return cell.coordinate
 
 
-def find_count_of_subjects(cell):
+def find_count_of_subjects(class_cell):
     count = 0
     for i in range(1, 20):
-        if sheet['B' + str(int(cell[1:]) + i)].value is None and count != 0:
+        if sheet['B' + str(int(class_cell[1:]) + i)].value is None and count != 0:
             break
         count += 1
     return count
 
 
+def find_serial_numbers(class_cell):
+    count = 0
+    serial_numbers = []
+    for i in range(1, 20):
+        if sheet['B' + str(int(class_cell[1:]) + i)].value is None and count != 0:
+            break
+
+        count += 1
+        serial_numbers.append(count)
+
+    return tuple(serial_numbers)
+
+
+def find_time_cells(class_cell):
+    time_cells = []
+
+    #count = 0
+    for i in range(1, 20):
+        for j in time_cells:
+
+            if sheet['С' + str(int(class_cell[1:]) + i)].value is None: # and count != 0:
+                break
+
+        #count += 1
+            time_cells.append()
+
+    return tuple(time_cells)
+
+
 def find_class_subjects(class_cell):
+    subjects = []
     for i in range(1, find_count_of_subjects(class_cell) + 1):
         cell = sheet[class_cell[0] + str(int(class_cell[1:]) + i)]
         if isinstance(cell, MergedCell):
             print(get_merged_cell_value(sheet, cell))
         else:
             print(cell.value)
-
-if number == 1 or number == 2 or number == 3:
-    number += 2
+    return tuple(subjects)
 
 
-#cell = find_class_cell(your_class)
-#find_class_subjects(cell)
+def create_a_schedule(class_cell):
+    return zip(find_serial_numbers(class_cell), find_time_cells(class_cell),  find_class_subjects(class_cell))
+
+
+cell = find_class_cell(your_class)
+find_class_subjects(cell)
+#create_a_schedule(cell)
+
+
+
+#print(*find_serial_numbers(cell))
+print(find_time_cells(cell))
+
